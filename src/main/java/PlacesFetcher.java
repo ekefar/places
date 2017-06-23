70d259b766d0dc843679b037d73d7a2089a4d85d
ekefar <ekefar@gmail.com>
2017-06-24 02:29:15 +0300

    Add Place DTO into model.

--- src/main/java/PlacesFetcher.java
+++ src/main/java/PlacesFetcher.java
@@ -6,7 +6,12 @@ import com.google.maps.model.LatLng;
 import com.google.maps.model.PlaceDetails;
 import com.google.maps.model.PlaceType;
 import com.google.maps.model.PlacesSearchResponse;
+import model.Place;
+import model.Location;
+import model.Place.Type;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,26 +21,41 @@ public class PlacesFetcher {
 
     private static final String KEY = "AIzaSyAd-eoNEpt5faRRvZribmZxha6VrPRcOIY";
 
-    public static void fetchPlaces() {
+    public static List<Place> fetchPlaces(Location location, Type type,  int radius) {
         GeoApiContext context = getGeoContext();
 
-        final LatLng location = new LatLng(50.034490, 36.220569);
-        final NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, location);
-        request.type(PlaceType.BANK).radius(1000);
+        final LatLng latLng = new LatLng(location.getLat(), location.getLng());
+        final NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, latLng);
+        request.type(PlaceType.BANK).radius(radius);
 
         final Optional<PlacesSearchResponse> placesSearchResponse = searchForPlaces(request);
 
         System.out.println(placesSearchResponse.get());
+
+        return new LinkedList<>();
     }
 
 
-
-    public static void fetchPlace(final String id) {
+    public static Optional<Place> fetchPlace(final String id) {
         final GeoApiContext context = getGeoContext();
         final PlaceDetailsRequest request = PlacesApi.placeDetails(context, id);
         final Optional<PlaceDetails> placeDetails = getPlaceDetails(request);
-        System.out.println(placeDetails.get());
 
+        if(placeDetails.isPresent()) {
+            final PlaceDetails place = placeDetails.get();
+            final LatLng location = place.geometry.location;
+            return Optional.ofNullable(new Place.Builder().setId(id)
+                    .setAddress(place.formattedAddress)
+                    .setLocation(new Location(location.lat, location.lng))
+                    .setName(place.name)
+                    .setPhoneNumber(place.formattedPhoneNumber)
+                    .setOpeningHours(place.openingHours.toString())
+                    .setMapUrl(place.url.toString())
+                    .setWebsiteUrl(place.website.toString())
+                    .build());
+        }
+
+        return Optional.empty();
     }
 
     private static Optional<PlaceDetails> getPlaceDetails(PlaceDetailsRequest request) {
---
+++ src/main/java/model/Place.java
@@ -0,0 +1,120 @@
+package model;
+
+/**
+ * @author : Alexander Serebriyan
+ */
+public class Place {
+
+    private String id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private Location location;
+    private String openingHours;
+    private String mapUrl;
+    private String websiteUrl;
+
+    private Place(String id, String name, String address, Location location, String phoneNumber,
+                  String openingHours, String mapUrl, String websiteUrl) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.location = location;
+        this.openingHours = openingHours;
+        this.mapUrl = mapUrl;
+        this.websiteUrl = websiteUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getMapUrl() {
+        return mapUrl;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public enum Type {
+        BANK
+    }
+
+    public static class Builder {
+        private String id;
+        private String name;
+        private String address;
+        private String phoneNumber;
+        private Location location;
+        private String openingHours;
+        private String mapUrl;
+        private String websiteUrl;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setLocation(Location location) {
+            this.location = location;
+            return this;
+        }
+
+        public Builder setMapUrl(String mapUrl) {
+            this.mapUrl = mapUrl;
+            return this;
+        }
+
+        public Builder setWebsiteUrl(String websiteUrl) {
+            this.websiteUrl = websiteUrl;
+            return this;
+        }
+
+        public Builder setOpeningHours(String openingHours) {
+            this.openingHours = openingHours;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+
+
+        public Place build() {
+            return new Place(id, name, address, location, phoneNumber, openingHours, mapUrl, websiteUrl);
+        }
+    }
+}
