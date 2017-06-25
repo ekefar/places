d327792368cf2a4cc8924830688a3435df529a81
ekefar <ekefar@gmail.com>
2017-06-26 00:59:27 +0300

    Add webapp config class.

--- service/src/main/java/com/places/service/read/PlacesReader.java
+++ service/src/main/java/com/places/service/read/PlacesReader.java
@@ -1,7 +1,22 @@
 package com.places.service.read;
 
+import com.places.model.entity.Place;
+import com.places.model.repository.PlacesRepository;
+
+import java.util.List;
+
 /**
  * @author : Alexander Serebriyan
  */
 public class PlacesReader {
+
+    private PlacesRepository repository = new PlacesRepository();
+
+    public List<Place> list() {
+        return repository.find();
+    }
+
+    public Place byId(String id){
+        return repository.find(id);
+    }
 }
--- webapp/src/main/java/com/places/web/controller/HomeController.java
+++ webapp/src/main/java/com/places/web/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.places.web;
+package com.places.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
