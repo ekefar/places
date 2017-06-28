6caec047a8007b11e388e6d2843b4cc59907f37d
ekefar <ekefar@gmail.com>
2017-06-28 04:00:48 +0300

    save() and find() repository methods.

--- model/src/main/java/com/places/model/repository/PlacesRepository.java
+++ model/src/main/java/com/places/model/repository/PlacesRepository.java
@@ -2,11 +2,16 @@ package com.places.model.repository;
 
 import com.places.model.entity.Place;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Collection;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 /**
  * @author : Alexander Serebriyan
@@ -23,6 +28,14 @@ public class PlacesRepository {
 
     public void save(Place place) {
         mongoTemplate.save(place);
+    }
+
+    public void save(Collection<Place> place) {
+        mongoTemplate.save(place);
+    }
+
+    public Place find(String id) {
+        return mongoTemplate.findOne(query(where("id").is(id)), Place.class);
     }
 
 }
