663ef43c138912676b2ada68da13f73f9e579a90
ekefar <ekefar@gmail.com>
2017-07-10 02:39:12 +0300

    Move bucket settings to properties.

--- parser/src/main/java/com/places/parser/SpringConfig.java
+++ parser/src/main/java/com/places/parser/SpringConfig.java
@@ -1,13 +1,8 @@
 package com.places.parser;
 
-import com.mongodb.MongoClient;
-import com.places.parser.service.photo.PhotosPersister;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * @author : Alexander Serebriyan
@@ -15,7 +10,16 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 @Import(com.places.model.SpringConfig.class)
 @ComponentScan(basePackages = "com.places.parser")
+@PropertySource("classpath:mongo.properties")
 public class SpringConfig {
 
+
+    @Value("${active.bucket}")
+    private String bucket;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 }
