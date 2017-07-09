875750b2552f6a76e8b311b80c2bc4082081dee4
ekefar <ekefar@gmail.com>
2017-07-10 02:36:20 +0300

    Persistence to S3.

--- parser/src/main/java/com/places/parser/service/photo/S3PhotosPersister.java
+++ parser/src/main/java/com/places/parser/service/photo/S3PhotosPersister.java
@@ -1,6 +1,15 @@
 package com.places.parser.service.photo;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author : Alexander Serebriyan
@@ -8,8 +17,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class S3PhotosPersister implements PhotosPersister {
 
-    @Override
-    public void persist(String path, byte[] content) {
+    @Value("${active.bucket}")
+    private String bucket;
 
+    @Override
+    public void persist(String key, byte[] content) {
+        final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider()).build();
+        final File file = bytesToFile(content);
+        s3Client.putObject(new PutObjectRequest(bucket, key, file));
+    }
+
+    private File bytesToFile(byte[] bytes) {
+        try {
+            final File file = new File(String.valueOf(System.nanoTime()));
+            FileUtils.writeByteArrayToFile(file, bytes);
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
